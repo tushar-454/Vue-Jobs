d4aba93ecf42481a15e4ae5104fe287939229ccf
tushar-454 <imtushar454@gmail.com>
2024-07-04 14:00:44 +0600

    data changed with based on multiple role and condition

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,31 @@
 export default {
   data() {
     return {
-      mesage: 'Hello vue',
+      name: 'John Dou',
+      status: 'active',
+      tasks: ['Task-1', 'Task-2', 'Task-3'],
     };
   },
   methods: {
-    clickMe() {
-      this.mesage = 'I am changed value';
+    changeStatus() {
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending') {
+        this.status = '';
+      } else {
+        this.status = 'active';
+      }
     },
   },
 };
 </script>
 
 <template>
-  <h1>{{ mesage }}</h1>
-  <button @click="clickMe">Click Me</button>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">{{ name }} is Active</p>
+  <p v-else-if="status === 'pending'">{{ name }} in pending</p>
+  <p v-else="status">{{ name }} in not available</p>
+  <button @click="changeStatus">Click Status</button>
 </template>
 
 <style scoped>
