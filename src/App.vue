14a9889bcb570f4d85ce5df9730a78e85fc860fb
tushar-454 <imtushar454@gmail.com>
2024-07-04 14:02:41 +0600

    vue js array loop

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,15 @@ export default {
   <p v-else-if="status === 'pending'">{{ name }} in pending</p>
   <p v-else="status">{{ name }} in not available</p>
   <button @click="changeStatus">Click Status</button>
+  <h2>Tasks</h2>
+  <ul>
+    <li
+      v-for="(task, index) in tasks"
+      :key="index"
+    >
+      {{ task }}
+    </li>
+  </ul>
 </template>
 
 <style scoped>
