f4d36b259aa52a1e0767b2e2cfb3358159629600
tushar-454 <imtushar454@gmail.com>
2024-07-09 09:18:59 +0600

    refactor: Add Tailwind CSS configuration and styles for Header component

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,7 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-
-const name = ref('John Dou');
-const status = ref('active');
-const tasks = ref(['Task-1', 'Task-2', 'Task-3']);
-const newTask = ref('');
-const changeStatus = () => {
-  if (status.value === 'active') {
-    status.value = 'pending';
-  } else if (status.value === 'pending') {
-    status.value = '';
-  } else {
-    status.value = 'active';
-  }
-};
-const addTask = () => {
-  tasks.value.push(newTask.value);
-  newTask.value = '';
-};
-const deleteTask = (index) => {
-  tasks.value.splice(index, 1);
-};
-
-// fetch data from api
-
-onMounted(async () => {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-    const todos = await response.json();
-    tasks.value = todos.map((todo) => todo.title);
-    loading = false;
-  } catch (error) {
-    console.log(error);
-  } finally {
-  }
-});
+import Header from './components/Header.vue';
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status === 'active'">{{ name }} is Active</p>
-  <p v-else-if="status === 'pending'">{{ name }} in pending</p>
-  <p v-else="status">{{ name }} in not available</p>
-  <button @click="changeStatus">Click Status</button>
-  <div>
-    <form @submit.prevent="addTask">
-      <label for="newTask">New Task: </label>
-      <input
-        type="text"
-        id="newTask"
-        name="newTask"
-        v-model="newTask"
-      />
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-  <h2>Tasks</h2>
-
-  <ul>
-    <li
-      v-for="(task, index) in tasks"
-      :key="index"
-    >
-      <span>
-        {{ task }}
-        <button @click="deleteTask(index)">X</button>
-      </span>
-    </li>
-  </ul>
+  <Header />
 </template>
-
-<style scoped>
-h1 {
-  color: red;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<script setup>
+import logo from '@/assets/images/logo.png';
+</script>
+
+<template>
+  <nav class="border-b border-green-500 bg-green-700">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex h-20 items-center justify-between">
+        <div
+          class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+        >
+          <!-- Logo -->
+          <a class="mr-4 flex flex-shrink-0 items-center" href="index.html">
+            <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+            <span class="ml-2 hidden text-2xl font-bold text-white md:block"
+              >Vue Jobs</span
+            >
+          </a>
+          <div class="md:ml-auto">
+            <div class="flex space-x-2">
+              <a
+                href="index.html"
+                class="rounded-md bg-green-900 px-3 py-2 text-white hover:bg-gray-900 hover:text-white"
+                >Home</a
+              >
+              <a
+                href="jobs.html"
+                class="rounded-md px-3 py-2 text-white hover:bg-green-900 hover:text-white"
+                >Jobs</a
+              >
+              <a
+                href="add-job.html"
+                class="rounded-md px-3 py-2 text-white hover:bg-green-900 hover:text-white"
+                >Add Job</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style></style>
